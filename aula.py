b0b2f9ed729e8cd69eb8237d9e7e92ec36ebebed
guilherme lemos <guilmss.dev@gmail.com>
2024-09-15 16:09:42 -0300

    primeiro commit

---
+++ aula.py
@@ -0,0 +1,22 @@
+# numero1 = int(input("informe o primeiro numero:"))
+# numero2 = int(input("informe o segundo numero:"))
+# print("informe o primeiro numero:{numero1} ","informe o segundo numero:{numero2}")
+
+# mult = numero1 * numero2
+
+# print(f"o resultado da multiplicação foi: {mult}")
+
+
+lista = [4,5,6,'GUI']
+print(lista)
+print(lista[2])
+
+nome = lista[3]
+print(nome)
+
+
+gui = {"irmao_velho":"paulo","pai":"airton","irmao_novo":"john"}
+print(gui["pai"])
+
+op = input("digite: ")
+print(gui[op])
